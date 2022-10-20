bf5834c88574e62ce57b49020af0b6a390adfc7d
Anastasya <gavrilovaanastasya@yandex.ru>
2022-10-20 14:52:44 +0300

    Принимает число, возвращает сумму его цифр

---
+++ Seminar9.3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Принимает число, возвращает сумму его чисел.
+
+Console.WriteLine("Введите число n: ");
+int n = int.Parse(Console.ReadLine());
+
+int RecSumNumbers(int n)
+{
+  if(n==0)
+  {
+    return 0;
+  }
+  return n%10 + RecSumNumbers(n/10);
+}
+
+Console.WriteLine(RecSumNumbers(n));
