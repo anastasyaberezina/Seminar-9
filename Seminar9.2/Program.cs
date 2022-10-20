2aaed3e61c51dd150f9744ae1a99aab526b4a97c
Anastasya <gavrilovaanastasya@yandex.ru>
2022-10-20 14:30:51 +0300

    Показать все цифры в промежутке от m до n.

---
+++ Seminar9.2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Показать все цифры в промежутке от m до n.
+
+Console.WriteLine("Введите число m: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число n: ");
+int n = int.Parse(Console.ReadLine());
+
+string Recurs(int m, int n)
+{
+    if (m == n)
+    {
+        return n.ToString();
+    }
+    return (m + " " + Recurs(m + 1, n));
+}
+
+Console.WriteLine(Recurs(m,n));
