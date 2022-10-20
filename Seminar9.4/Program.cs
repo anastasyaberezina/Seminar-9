028abcd5c008e900df3813aa0d14d6a3d3b85bcb
Anastasya <gavrilovaanastasya@yandex.ru>
2022-10-20 15:04:45 +0300

    Принимает A и B. Возводит число A в степень B.

---
+++ Seminar9.4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Принимает A и B. Возводит число A в степень B.
+
+Console.WriteLine("Введите число A: ");
+int A = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число B: ");
+int B= int.Parse(Console.ReadLine());
+
+int RecPow(int m, int n)
+{
+  if(n==0)
+  {
+    return 1;
+  }
+  return m * RecPow(m, n-1);
+}
+
+Console.WriteLine(RecPow(A,B));
