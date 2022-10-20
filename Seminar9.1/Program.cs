d6759f85f60e70bd3d843879994c695fb8ba75c5
Anastasya <gavrilovaanastasya@yandex.ru>
2022-10-20 14:26:32 +0300

    Задать значение n. Вывести все цифры в промежутке от 1 до n.

---
+++ Seminar9.1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задать значение n. Вывести все цифры в промежутке от 1 до n.
+
+Console.WriteLine("Введите число n: ");
+int n = int.Parse(Console.ReadLine());
+
+string Recurs(int minValue, int n)
+{
+    if (minValue == n)
+    {
+        return n.ToString();
+    }
+    return (minValue + " " + Recurs(minValue + 1, n));
+}
+
+Console.WriteLine(Recurs(1,n));
