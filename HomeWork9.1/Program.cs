dee2b030aaaffc0fe936474830795fb0d896acc6
Anastasya <gavrilovaanastasya@yandex.ru>
2022-10-21 15:31:59 +0300

    выведет все натуральные числа в промежутке от N до 1

---
+++ HomeWork9.1/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+Выполнить с помощью рекурсии.*/
+
+Console.WriteLine("Введите число n: ");
+int number = int.Parse(Console.ReadLine());
+
+string Recurs(int number)
+{
+    if (number==1)
+    {
+        return number.ToString();
+    }
+    return (number+" "+Recurs(number-1));
+}
+
+Console.WriteLine(Recurs(number));
