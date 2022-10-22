0af87437e2334e8aaf41bb76568c4b7d03c1eb9b
Anastasya <gavrilovaanastasya@yandex.ru>
2022-10-22 10:47:21 +0300

    найдёт сумму натуральных элементов в промежутке от M до N

---
+++ HomeWork9.2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.WriteLine("Введите число M: ");
+int M = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+
+int Recurs(int m, int n)
+{
+    if (m == 0)
+    {
+        return (n * (n + 1)) / 2;
+    }
+    if (n == 0)
+    {
+        return (m * (m + 1)) / 2;
+    }
+    if (m == n)
+    {
+        return m;
+    }
+    if (m < n)
+    {
+        return n + Recurs(m, n - 1);
+    }
+    if (m > n)
+    {
+        return n + Recurs(m, n + 1);
+    }
+    return Recurs(m, n);
+}
+
+Console.WriteLine(Recurs(M, N));
+
